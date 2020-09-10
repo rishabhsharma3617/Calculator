95e4365559b624f438c4076e36ff929b26355614
Rishabh Sharma <rishabhs@geekyants.com>
2020-09-10 15:45:38 +0530

    Removed the bugs

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 var inputValue = 0
 var a =0;
 var b =0;
@@ -12,33 +10,32 @@ var currVal = 0
 var result =0
 var equalPressed = false
 var justStart = false
+var flagCustom = false
 
 
-
-function disableKeyboard(event){
-        event.preventDefault()
-}
+function disableKeyboard(event){event.preventDefault()}
 window.onload = () => {
 function customReset()
 {
-    
-    setTimeout(() => document.getElementById('in-number').value = '',1000)
+    flagCustom = true
+    setTimeout(() => {
+        document.getElementById('in-number').value = '0'
+        document.getElementById('in-number').readOnly = false
+    },1200)
     flag='false'
     itemsArray = []
     operationsArray = []
     currVal = 0
+    result =0
     operatorElement = null
     document.getElementById('equalSign').setAttribute('style','background-color: rgb(24, 68, 163);')
 }
     
 function doCalc()
 {
-   
-    result = itemsArray[0]
-
+   result = itemsArray[0]
    for(var i=0;i<operationsArray.length;i++)
    {
-       
        if(operationsArray[i] === '+')
            result =  result + itemsArray[i+1] 
        if(operationsArray[i] === '*')
@@ -47,80 +44,102 @@ function doCalc()
             result = result/itemsArray[i+1]
        if(operationsArray[i] === '-')
            result = result - itemsArray[i+1]
-    
-
-   }
-   document.getElementById('in-number').value = result
+   } 
    if(result === Infinity)
    {
-      customReset()
+    document.getElementById('in-number').value = 'INFINITY'
+    document.getElementById('in-number').readonly = true
+    customReset()
    }
-   
-   document.getElementById('in-number').value = result
-   
-   
+   else if(Number.isNaN(result))
+   {
+    console.log('hhhhh')
+    document.getElementById('in-number').value = 'NOT DEFINED'
+    console.log(document.getElementById('in-number'))
+    document.getElementById('in-number').readOnly = true
+    console.log(document.getElementById('in-number').readOnly)
+    customReset()
+   }
+   else
+   {
+    document.getElementById('in-number').value = result
+   }  
 }
 
     document.getElementById('in-number').value = '0'
     var tdTags = document.getElementsByTagName('td')
     for(var i =0;i<tdTags.length ; i++)
-    {
+    {   
         var tm= tdTags[i].innerText
         var tmp = tm.slice()
         if(tmp === 'AC')
         {
-            tdTags[i].addEventListener('click', (event) => {
-                document.getElementById('in-number').value = ''
+                tdTags[i].addEventListener('click', (event) => {
+                document.getElementById('in-number').value = '0'
                 flag='false'
                 itemsArray = []
                 operationsArray = []
+                flagCustom = true
                 currVal = 0
+                if(operatorElement)
+                operatorElement.setAttribute('style' , '')
                 operatorElement = null
                 document.getElementById('equalSign').setAttribute('style','background-color: rgb(24, 68, 163);')
-              })
-           
+              })        
         }
+
         else if(tmp === '*' || tmp==='/' || tmp==='+' || tmp==='-')
         {
             tdTags[i].addEventListener('click', (event) => {
-                
-                document.getElementById('equalSign').setAttribute('style','background-color: rgb(24, 68, 163);')
-                operator = event.target.innerText
-                if(operatorElement)
-                operatorElement.setAttribute('style','')
-
-                if(flag === true)
+                if(flagCustom)
                 {
-                    document.getElementById('in-number').value = 0
-                    operationsArray.push(operator)
-                    if(!equalPressed)
-                    {
-                        itemsArray.push(currVal)
-                    }
-                
-                    currVal = 0
-                    document.getElementById('in-number').value = ''
-                    operatorElement = event.target
-                    flag=false
-                    event.target.setAttribute('style' , 'background-color: rgb(56, 109, 140)')
+                    itemsArray.push(0)
+                    flag = true
+                }
+                console.log(itemsArray , operationsArray)
+                if(document.getElementById('in-number').value.slice(-1) === '.')
+                {
+                    alert('Please type the correct number')
                 }
                 else
                 {
-                    alert('Please type in the correct value')
-                }
- 
+                  
+                
+                        document.getElementById('equalSign').setAttribute('style','background-color: rgb(24, 68, 163);')
+                        operator = event.target.innerText
+                        if(operatorElement)
+                        operatorElement.setAttribute('style','')
+        
+                        if(flag === true)
+                        {
+                            document.getElementById('in-number').value = 0
+                            operationsArray.push(operator)
+                            if(!equalPressed)
+                            {
+                                itemsArray.push(currVal)
+                            }
+                        
+                            currVal = 0
+                            document.getElementById('in-number').value = ''
+                            operatorElement = event.target
+                            flag=false
+                            event.target.setAttribute('style' , 'background-color: rgb(56, 109, 140)')
+                        }
+                        else
+                        {
+                            alert('Please type in the correct value')
+                        }
+                    
+                }         
               })
         }
         else if(tmp === '=')
-        {
-
+        {          
             tdTags[i].addEventListener('click', (event) => {
-                console.log(document.getElementById('in-number').value)
-                
-                console.log(currVal)
                 if(result === Infinity)
-                {    console.log('jhgh')
+                {   
                     itemsArray = []
+                    console.log('kjkjkj')
                     operationsArray = []
                     flag='false'
                     itemsArray = []
@@ -130,11 +149,8 @@ function doCalc()
                     setTimeout(() => document.getElementById('in-number').value = '', 1500)
                 }
                 else
-                {
-                    if(itemsArray.length === 0 )
-                alert('Please Type in the values first')
-
-                if(flag===true)
+                {          
+                if(flag===true && operationsArray.length!==0)
                 {
                     
                     itemsArray.push(currVal)
@@ -147,28 +163,45 @@ function doCalc()
                 }
                 else
                 {
-                    alert('Please give us the correct value')
+                    alert('Please give us the correct values')
                 }
                 }
-
-                
-
               })
             
         }
         else
         {
             tdTags[i].addEventListener('click', (event) => {
-                
+                flagCustom = false
                 var clickedButton = event.target.innerText
-                tmp = document.getElementById('in-number').value + clickedButton
-                document.getElementById('in-number').value = tmp
                 
-                currVal = parseFloat(tmp)
-                
-                equalPressed = false
-                
-                flag = true
+                var validateDot = document.getElementById('in-number').value
+                if(validateDot.length >=8)
+                {
+                    alert('Digits Limit Exceeded')
+                    document.getElementById('in-number').value = '0'
+                    customReset()
+                }
+                else
+                {
+                    if(validateDot.includes('.') && clickedButton==='.')
+                    {
+                        alert('Please type the correct number')
+                        
+                    }
+                    else
+                    {
+                    tmp = document.getElementById('in-number').value + clickedButton
+                    document.getElementById('in-number').value = tmp
+                    if(operatorElement)
+                    operatorElement.setAttribute('style' , '')
+                    currVal = parseFloat(tmp)
+                    equalPressed = false
+                    flag = true
+                    
+                    }
+                }
+            
                 
               })
         }
